c96b02084dff671f08c93351651a71aa96fbf595
ablahum <ablahum@proton.me>
2025-08-01 19:53:42 +0700

    configure auth & middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/common/header'
 import type { Metadata } from 'next'
 import { Source_Sans_3 as FontSans } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const fontSans = FontSans({
   variable: '--font-sans',
@@ -21,16 +22,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${fontSans.variable} font-sans antialiased`}>
-        <div className='relative flex min-h-screen flex-col'>
-          <Header />
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${fontSans.variable} font-sans antialiased`}>
+          <div className='relative flex min-h-screen flex-col'>
+            <Header />
 
-          <main className='flex-1'>{children}</main>
+            <main className='flex-1'>{children}</main>
 
-          <Footer />
-        </div>
-      </body>
-    </html>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
