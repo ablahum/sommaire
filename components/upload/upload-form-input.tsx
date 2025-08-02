ed6b4d75637718aa4e8f8212178f6e9879a23924
ablahum <ablahum@proton.me>
2025-08-02 19:04:12 +0700

    feat(upload): configure file upload using  uploadthing

---
+++ components/upload/upload-form-input.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { Loader2 } from 'lucide-react'
+import { forwardRef } from 'react'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+
+interface UploadFormInputProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  isLoading: boolean
+}
+
+export const UploadFormInput = forwardRef<HTMLFormElement, UploadFormInputProps>(({ onSubmit, isLoading }, ref) => {
+  return (
+    <form
+      ref={ref}
+      className='flex flex-col gap-6'
+      onSubmit={onSubmit}
+    >
+      <div className='flex justify-end items-center min-[400px]:gap-1.5 gap-2 min-[400px]:flex-row flex-col'>
+        <Input
+          id='file'
+          type='file'
+          name='file'
+          accept='application/pdf'
+          required
+          className={cn(isLoading && 'opacity-50 cursor-not-allowed')}
+          disabled={isLoading}
+        />
+
+        <Button
+          disabled={isLoading}
+          className='w-full min-[400px]:w-auto bg-gradient-to-r 
+              from-slate-900 to-rose-500 hover:from-rose-500 
+              hover:to-slate-900 hover:text-white text-white
+              transition-all duration-300'
+        >
+          {isLoading ? (
+            <>
+              <Loader2
+                className='mr-2 h-4 w-4 
+              animate-spin'
+              />{' '}
+              Processing...
+            </>
+          ) : (
+            'Upload your PDF'
+          )}
+        </Button>
+      </div>
+    </form>
+  )
+})
+
+UploadFormInput.displayName = 'UploadFormInput'
+
+export default UploadFormInput
---
+++ components/upload/upload-form.tsx
@@ -0,0 +1,124 @@
+'use client'
+import { z } from 'zod'
+import UploadFormInput from './upload-form-input'
+import { useUploadThing } from '@/utils/uploadthing'
+import { toast } from 'sonner'
+import { generatePdfSummary, storePdfSummaryAction } from '@/actions/upload-actions'
+import { useRef, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: 'Invalid file' })
+    .refine(file => file.size <= 20 * 1024 * 1024, 'File size must be less than 20MB')
+    .refine(file => file.type.startsWith('application/pdf'), 'File must be a PDF')
+})
+
+export default function UploadForm() {
+  const formRef = useRef<HTMLFormElement>(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+
+  const { startUpload, routeConfig } = useUploadThing('pdfUploader', {
+    onClientUploadComplete: () => {
+      console.log('uploaded successfully!')
+    },
+
+    onUploadError: err => {
+      console.error('error occurred while uploading', err)
+      toast.error('Error occurred while uploading', {
+        description: err.message
+      })
+    },
+
+    onUploadBegin: data => {
+      console.log('upload has begun for', data)
+    }
+  })
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      setIsLoading(true)
+
+      const formData = new FormData(e.currentTarget)
+      const file = formData.get('file') as File
+      const validatedFields = schema.safeParse({ file })
+      console.log(validatedFields)
+
+      if (!validatedFields.success) {
+        toast.error('❌ Something went wrong', {
+          description: validatedFields.error.flatten().fieldErrors.file?.[0] ?? 'Invalid file'
+        })
+
+        setIsLoading(false)
+        return
+      }
+
+      toast('📑 Uploading PDF', {
+        description: 'We are Uploading your PDF...'
+      })
+
+      const uploadResponse = await startUpload([file])
+      if (!uploadResponse || uploadResponse.length === 0) {
+        toast.error('❌ Something went wrong', {
+          description: 'Please use a different file'
+        })
+
+        setIsLoading(false)
+        return
+      }
+
+      toast('📑 Processing PDF', {
+        description: 'Hang tight! Our AI is reading through your document! ✨'
+      })
+
+      const uploadFileUrl = uploadResponse[0].serverData.fileUrl
+      const summary = await generatePdfSummary({
+        fileUrl: uploadFileUrl,
+        fileName: file.name
+      })
+
+      // const { data = null, message = null } = summary || {}
+
+      // if (data) {
+      //   let storeResult: any
+      //   toast('📑 Saving PDF', {
+      //     description: 'Hang tight! We are Saving your Summary! 💫'
+      //   })
+
+      //   if (data.summary) {
+      //     //save the summary to the database
+      //     storeResult = await storePdfSummaryAction({
+      //       summary: data.summary,
+      //       fileUrl: uploadFileUrl,
+      //       title: data.title,
+      //       fileName: file.name
+      //     })
+      //     toast('🤩 Summary Generated', {
+      //       description: 'Your PDF has been Successfully summarized and saved! 💫'
+      //     })
+      //     formRef.current?.reset()
+      //     // Step 6: redirect summary to the user
+      //     router.push(`/summaries/${storeResult.data.id}`)
+      //   }
+      // }
+    } catch (error) {
+      setIsLoading(false)
+      formRef.current?.reset()
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <div className='flex flex-col gap-8 w-full max-w-2xl mx-auto'>
+      <UploadFormInput
+        isLoading={isLoading}
+        ref={formRef}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  )
+}
