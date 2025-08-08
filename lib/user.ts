b5b971e2b59499540761a7304e857f765808ae35
ablahum <ablahum@proton.me>
2025-08-08 19:57:52 +0700

    feat(dashboard): upload file based on user's plan

--- lib/user.ts
+++ lib/user.ts
@@ -1,4 +1,6 @@
+import { pricingPlans } from '@/utils/contants'
 import { getDbConnection } from './db'
+import { getUserUploadCount } from './summaries'
 
 export async function getPriceIdForActiveUser(email: string) {
   const sql = await getDbConnection()
@@ -6,4 +8,16 @@ export async function getPriceIdForActiveUser(email: string) {
     SELECT price_id FROM users WHERE email = ${email} AND status = 'active'
   `
   return query?.[0]?.price_id || null
+}
+
+export async function hasReachedUploadLimit(userId: string) {
+  const uploadCount = await getUserUploadCount(userId)
+
+  const priceId = await getPriceIdForActiveUser(userId)
+
+  const isPro = pricingPlans.find(plan => plan.priceId === priceId)?.id === 'pro'
+
+  const uploadLimit: number = isPro ? 1000 : 3
+
+  return { hasReachedLimit: uploadCount >= uploadLimit, uploadLimit }
 }
