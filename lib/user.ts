1c2f5d7f8283ca7ffe50d274ad96e481b9c4f143
ablahum <ablahum@proton.me>
2025-08-09 21:04:35 +0700

    feat(dashboard): show "upgrade required" if user not subs yet

--- lib/user.ts
+++ lib/user.ts
@@ -4,19 +4,35 @@ import { getUserUploadCount } from './summaries'
 
 export async function getPriceIdForActiveUser(email: string) {
   const sql = await getDbConnection()
+
   const query = await sql`
-    SELECT price_id FROM users WHERE email = ${email} AND status = 'active'
+    SELECT price_id
+    FROM users
+    WHERE email = ${email}
+    AND status = 'active'
   `
+
   return query?.[0]?.price_id || null
+}
+
+export async function hasActivePlan(email: string) {
+  const sql = await getDbConnection()
+
+  const query = await sql`
+    SELECT price_id, status
+    FROM users
+    WHERE email = ${email}
+    AND status = 'active'
+    AND price_id IS NOT NULL
+  `
+
+  return query && query.length > 0
 }
 
 export async function hasReachedUploadLimit(userId: string) {
   const uploadCount = await getUserUploadCount(userId)
-
   const priceId = await getPriceIdForActiveUser(userId)
-
   const isPro = pricingPlans.find(plan => plan.priceId === priceId)?.id === 'pro'
-
   const uploadLimit: number = isPro ? 1000 : 3
 
   return { hasReachedLimit: uploadCount >= uploadLimit, uploadLimit }
