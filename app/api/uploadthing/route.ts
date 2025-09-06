0ff0ff2177ed330b422940f128d721f89cd65a66
ablahum <ablahum@proton.me>
2025-09-06 19:13:37 +0700

    update uploadthing's configuration

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -4,7 +4,6 @@ import { ourFileRouter } from './core'
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
   config: {
-    token:
-      'eyJhcGlLZXkiOiJza19saXZlXzYzMjA4MDU2MWM2M2U1M2M5MmYwOTk5YmI2YjlmODk4ZWZjYWQ5Mzc4ODFhMjg0OTIzZTg4MGJhMWRlY2RmNWEiLCJhcHBJZCI6ImtsdnJuajFxaXMiLCJyZWdpb25zIjpbInNlYTEiXX0=',
+    token: process.env.UPLOADTHING_TOKEN,
   },
 })
